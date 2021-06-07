09a5b8fb19011f18bcbe552f7f7f1fa89edb6dcd
hosibito <hosibito@naver.com>
2021-06-07 19:24:16 +0900

    ROOM APP 전부진행

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.RoomType, models.Amenity, models.Facility, models.HouseRule)
+class ItemAdmin(admin.ModelAdmin):
+    """Item Admin Definition"""
+
+    list_display = ("name", "create", "update")
+
+
+@admin.register(models.Room)
+class RoomAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """ """
+
+    pass
--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,131 @@
 from django.db import models
+from django_countries.fields import CountryField
+from core import models as core_models
+from users import models as user_models
 
-# Create your models here.
+
+class AbstractItem(core_models.TimeStampedModel):
+    """Abstract Item"""
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class RoomType(AbstractItem):  # 하단설명 2 참조
+
+    """RoomType Model Definition"""
+
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["name", "create"]
+
+    """
+    Entire place, Private room, Hotel room, Shared room
+    """
+
+
+class Amenity(AbstractItem):
+
+    """Amenity Model Definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+
+class Facility(AbstractItem):
+
+    """Facility Model Definition"""
+
+    class Meta:
+        verbose_name = "시설"
+        verbose_name_plural = "시설들"
+
+
+class HouseRule(AbstractItem):
+
+    """HouseRule Model Definition"""
+
+    class Meta:
+        verbose_name = "House Rule"
+
+
+class Photo(core_models.TimeStampedModel):
+    """Photo Model Definition"""
+
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    # room = models.ForeignKey(Room, on_delete=models.CASCADE) Room 정의가 밑에있어서 에러
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)  # 하단설명 3 참조
+
+    def __str__(self):
+        return self.caption
+
+
+class Room(core_models.TimeStampedModel):
+
+    """Room Model Definition"""
+
+    name = models.CharField(max_length=140)
+    description = models.TextField()
+    country = CountryField()  # 하단 설명 1 참조
+    city = models.CharField(max_length=80)
+    price = models.IntegerField()
+    address = models.CharField(max_length=140)
+    gueste = models.IntegerField()
+    beds = models.IntegerField()
+    bedrooms = models.IntegerField()
+    baths = models.IntegerField()
+    check_in = models.TimeField()
+    check_out = models.TimeField()
+    instant_book = models.BooleanField(default=False)
+    host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
+    room_type = models.ForeignKey(RoomType, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+"""   1 
+https://github.com/SmileyChris/django-countries
+나라이름을 입력할수 있게 해준다.
+
+pipenv install django-countries
+Add "django_countries" to INSTALLED_APPS
+from django_countries.fields import CountryField
+"""
+
+"""  2 
+Entire place, Private room, Hotel room, Shared room 등의 옵션이 올곳
+
+모델에 초이스를 사용해서 직접 박아넣을수도 있다. 
+어찌보면 따로 만들고 어드민에서 추가안해줘도 되니 편할수도 있다.
+다만 저건 프로그래머의 방법이다. 프로그래머가 아닌 누군가도 추가할수 있으려면
+이렇게 만드는게 낫다. 협업시에도 유리하다. 
+ GENDER_MALE = "male"
+    GENDER_FRMALE = "female"
+    GENDER_OTHER = "other"
+
+    GENDER_CHOICES = (
+        (GENDER_MALE, "Male"),  # GENDER_MALE 은 DB에저장 Male은 form에 보여짐
+        (GENDER_FRMALE, "Female"),
+        (GENDER_OTHER, "Other"),
+    )
+"""
+
+""" 3
+ room = models.ForeignKey(Room, on_delete=models.CASCADE) 
+   Room 정의가 밑에있어서 에러 따라서 
+ room = models.ForeignKey("Room", on_delete=models.CASCADE) 
+  "Room" 으로 string 로 바꿔주면 된다. 
+   임포트도 필요없어 지는데.. 
+   https://nomadcoders.co/airbnb-clone/lectures/914 다시 볼것.
+
+"""
