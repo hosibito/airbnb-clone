4c91dbe5100e0655c91e301728af46703ff95349
hosibito <hosibito@naver.com>
2021-07-19 22:27:03 +0900

    #10 INTRODUCTION TO VIEWS AND URLS

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,14 @@
+# from datetime import datetime
 from django.shortcuts import render
 
-# Create your views here.
+from . import models
+
+
+def all_rooms(request):
+    # print(vars(request))
+    # print(dir(request))
+    # now = datetime.now()
+    # hungry = True
+    # return render(request, "all_rooms.html", context={"now": now, "hungry": hungry})
+    all_rooms = models.Room.objects.all()    
+    return render(request, "rooms/home.html", context={"rooms": all_rooms})
